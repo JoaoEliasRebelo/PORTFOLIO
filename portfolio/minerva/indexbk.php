f59636cf2e684555cce073dec6da38640c89a3c7
Diogo Henrique <66573169+alterbeto@users.noreply.github.com>
2020-11-25 00:47:28 +0000

    Add files via upload

---
+++ portfolio/minerva/indexbk.php
@@ -0,0 +1,181 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" prefix="og: http://ogp.me/ns#" lang="es" style="" class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths"><head><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"><meta charset="UTF-8"><title>Duarte Amaral Netto Photographs: 1999 — 2007 - Duarte Amaral Netto</title><meta name="description" content=""><link rel="pingback" href="http://www.duartenetto.com/xmlrpc.php"><link rel="shortcut icon" href="http://www.duartenetto.com/wp-content/themes/duarte/img/favicon.png"><link rel="apple-touch-icon-precomposed" href="http://www.duartenetto.com/wp-content/themes/duarte/img/favicon-retina.png"><link rel="stylesheet" href="http://www.duartenetto.com/wp-content/themes/duarte/css/video-js.css"><link rel="stylesheet" href="http://www.duartenetto.com/wp-content/themes/duarte/css/normalize.min.css"><link rel="stylesheet" href="http://www.duartenetto.com/wp-content/themes/duarte/css/bootstrap.min.css"><link rel="stylesheet" href="http://www.duartenetto.com/wp-content/themes/duarte/css/slick.css"><link rel="stylesheet" href="http://www.duartenetto.com/wp-content/themes/duarte/style.css"> <script src="http://www.duartenetto.com/wp-content/themes/duarte/js/modernizr-2.6.2-respond-1.1.0.min.js"></script> <!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <link rel="stylesheet" type="text/css" href="css/ie.css" media="all"/> <![endif]--><link rel="canonical" href="http://www.duartenetto.com/project/1999-2007/"><meta property="og:locale" content="es_ES"><meta property="og:type" content="article"><meta property="og:title" content="Photographs: 1999 — 2007 - Duarte Amaral Netto"><meta property="og:url" content="http://www.duartenetto.com/project/1999-2007/"><meta property="og:site_name" content="Duarte Amaral Netto"><meta property="og:image" content="http://www.duartenetto.com/wp-content/uploads/2014/09/Rumor-1-2000x1561.jpg"><meta property="og:image:width" content="2000"><meta property="og:image:height" content="1561"><meta name="twitter:card" content="summary"><meta name="twitter:title" content="Photographs: 1999 — 2007 - Duarte Amaral Netto"><meta name="twitter:image" content="http://www.duartenetto.com/wp-content/uploads/2014/09/Rumor-1.jpg"><link rel="dns-prefetch" href="//s.w.org"><style type="text/css">img.wp-smiley,img.emoji{display:inline!important;border:none!important;box-shadow:none!important;height:1em!important;width:1em!important;margin:0 .07em!important;vertical-align:-0.1em!important;background:none!important;padding:0!important}</style><link rel="stylesheet" id="fancybox-css" href="http://www.duartenetto.com/wp-content/plugins/easy-fancybox/fancybox/jquery.fancybox.min.css?ver=1.3.19" type="text/css" media="screen"><style id="fancybox-inline-css" type="text/css">#fancybox-content{border-color:rgba(10,10,30,0)}#fancybox-title,#fancybox-title-float-main{color:#fff}</style><link rel="stylesheet" id="page-transition-animsition-css" href="http://www.duartenetto.com/wp-content/plugins/page-transition/css/animsition.min.css?ver=1.3" type="text/css" media="all"> <script type="text/javascript" src="http://www.duartenetto.com/wp-includes/js/jquery/jquery.js?ver=1.12.4"></script> <script type="text/javascript" src="http://www.duartenetto.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script> <script type="text/javascript" src="http://www.duartenetto.com/wp-content/plugins/page-transition/js/jquery.animsition.min.js?ver=1.3"></script> <link rel="https://api.w.org/" href="http://www.duartenetto.com/wp-json/"><link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.duartenetto.com/xmlrpc.php?rsd"><link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.duartenetto.com/wp-includes/wlwmanifest.xml"><meta name="generator" content="WordPress 4.9.16"><link rel="shortlink" href="http://www.duartenetto.com/?p=284"><link rel="alternate" type="application/json+oembed" href="http://www.duartenetto.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fwww.duartenetto.com%2Fproject%2F1999-2007%2F"><link rel="alternate" type="text/xml+oembed" href="http://www.duartenetto.com/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fwww.duartenetto.com%2Fproject%2F1999-2007%2F&amp;format=xml"> <script type="text/javascript">jQuery( document ).ready( function($) {
+			$('.animsition').animsition({
+				inClass : 'fade-in',
+				outClass : 'fade-out',
+				inDuration : 400,
+				outDuration : 400,
+				loading : false,
+				touchSupport: false,
+				linkElement: '.animsition-link, a[href]:not([target="_blank"]):not([href^="http://www.duartenetto.com/project/1999-2007/#"]):not([href^="#"]):not([href*="javascript"]):not([href*=".jpg"]):not([href*=".jpeg"]):not([href*=".gif"]):not([href*=".png"]):not([href*=".mov"]):not([href*=".swf"]):not([href*=".mp4"]):not([href*=".flv"]):not([href*=".avi"]):not([href*=".mp3"]):not([href^="mailto:"]):not([class="no-animation"])'
+			});
+		});</script>
+		<style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+</style>
+    </head><body class="project-template-default single single-project postid-284"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><style>.el0:hover{color:rgba(255, 0, ,0,1)}.el1:hover{color:rgba(255, 0, 0,1)}.el2:hover{color:rgba(0, 255, 0,1)}.el3:hover{color:rgba(0, 0, 255, 1)}.el3:hover{color:rgba(0, 0, 255, 1)}</style><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="animsition" style="animation-duration: 0.4s; opacity: 1;"><div class="navbar clearfix" role="navigation"><div class="container-fluid "><div class="navbar-header sticked"><ul class="menu"><li class="logo-desktop"><a style="color:" class="logo" href="#"><span>João Elias Rebelo</span></a></li><li class="logo-mobile"><a style="color:" class="logo" href="http://www.duartenetto.com">João Elias Rebelo</a></li><li class="upper-menu"><ul class="menu">
+    <li class="menu-item element1 work "><p class="el1" href="portfolio">Portfólio</p><ul id="all-projects" class="sub-menu clearfix project-container col-sm-8 col-sm-offset-2 row works sticked" style="
+    background-color: lightgrey;
+"><li><div><b><a href="http://www.duartenetto.com/project/transfers-family-album/">Art Work</a></b><div><a href="portfolio/minerva/">Minerva</a></div></div></li><li><div><b><a href="http://www.duartenetto.com/project/transfers-family-album/">Commercial</a></b><div><a href="http://www.duartenetto.com/project/transfers-family-album/">Product</a></div></div></li></ul></li><li class="menu-item element2 "><p class="el2" href="http://www.duartenetto.com/exhibition/" style="
+    text-transform: none;
+">News</p></li><li class="menu-item element3  " style=""><p href="http://www.duartenetto.com/about/" style="    text-transform: none;" class="el3">About</p></li><li class="instagram-mobile"><a target="_blank" href="https://www.instagram.com/duarte.netto/">instagram</a></li><li class="contact-mobile"><a href="mailto:contact@duartenetto.com">contact</a></li></ul></li></ul><div class="toggle-menu">Menu</div><div class="close"><img src="http://www.duartenetto.com/wp-content/themes/duarte/img/close.svg"></div></div></div><div class="divider"></div></div><div id="project" class="project2 container-fluid" style="background-color:#efefef;"><div class="wide"><div class="project-title-description text-center"><h1 class="project-title">Minerva</h1><div class="project-description text-left"><div class="project-block project-text col-md-8 col-md-offset-2 clearfix"><p>Uma descrição que</p><p>um certo burro ha de fazer</p><p>&nbsp;</p></div></div></div></div>
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="janelas.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="janelas1.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="janelas2.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+</body>
+</html>
