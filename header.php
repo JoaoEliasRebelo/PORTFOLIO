977e8257aa38b8e17a7713b149f52f2865da926a
Diogo Henrique <66573169+alterbeto@users.noreply.github.com>
2020-11-25 00:05:58 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,11 @@
+<div class="navbar clearfix" role="navigation"><div class="container-fluid " style="
+    /* background-color: lightgrey; */
+"><div class="navbar-header"><ul class="menu"><li class="logo-desktop"><a style="color:" class="logo" href="#"><span>João Elias Rebelo</span></a></li><li class="logo-mobile"><a style="color:" class="logo" href="http://www.duartenetto.com">João Elias Rebelo</a></li><li class="upper-menu"><ul class="menu">
+    <li class="menu-item element1 work "><p class="el1" href="portfolio">Portfólio</p><ul id="all-projects" class="sub-menu clearfix project-container col-sm-8 col-sm-offset-2 row works" style="
+    background-color: lightgrey;
+"><li><div><b><a href="http://www.duartenetto.com/project/transfers-family-album/">Art Work</a></b><div><a href="portfolio/minerva/">Minerva</a></div></div></li><li><div><b><a href="http://www.duartenetto.com/project/transfers-family-album/">Commercial</a></b><div><a href="http://www.duartenetto.com/project/transfers-family-album/">Product</a></div></div></li></ul></li><li class="menu-item element2 "><p class="el2" href="http://www.duartenetto.com/exhibition/" style="
+    text-transform: none;
+">News</p></li><li class="menu-item element3  " style="
+"><p href="http://www.duartenetto.com/about/" style="
+    text-transform: none;
+" class="el3">About</p></li><li class="instagram-mobile"><a target="_blank" href="https://www.instagram.com/duarte.netto/">instagram</a></li><li class="contact-mobile"><a href="mailto:contact@duartenetto.com">contact</a></li></ul></li></ul><div class="toggle-menu">Menu</div><div class="close"><img src="http://www.duartenetto.com/wp-content/themes/duarte/img/close.svg"></div></div></div><div class="divider"></div></div>
